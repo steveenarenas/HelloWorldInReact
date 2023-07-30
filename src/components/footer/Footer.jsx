6e33a3c60e23fa63baaa477806287624d8bb2e8d
Steven Arenas <steveenarenas@gmail.com>
2023-07-30 15:35:59 -0500

    Se realizo hola mundo en react con nav y footer

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,13 @@
+import "./Footer.css"
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <p>
+                copyright 2030 hecho con amor :3
+            </p>
+        </div>
+    );
+}
+
+export default Footer;
---
+++ src/components/nav/Nav.jsx
@@ -0,0 +1,19 @@
+import "./Nav.css"
+
+const Nav = () => {
+    return (
+        <div className="nav">
+            <a>
+                inicio
+            </a>
+            <a>
+                about
+            </a>
+            <a>
+                pay
+            </a>
+        </div>
+    );
+}
+
+export default Nav;
